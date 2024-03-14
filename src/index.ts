cfbcb1e626589ffbe1a11c13c9c2d3b6b4a1bc9a
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-03-14 19:21:54 -0300

    login route test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import fs from "fs";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -6,6 +7,17 @@ const port = process.env.PORT || 3000;
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript Express!');
 });
+
+app.get("/login", (req: Request, res: Response) => {
+  fs.readFile("pseudoviews/login.html", "utf-8", (err, data : string) => {
+    if(err) {
+      res.send("not loaded!!!!");
+      return;
+    }
+
+    res.send(data);
+  })
+})
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
