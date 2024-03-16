3dcac1b0dcf48583eec91fadeda3508f710ab1ad
José <josesand99v@gmail.com>
2024-03-16 20:36:51 -0300

    Add zod validation middleware and remove unused ZodParser

---
+++ src/middleware/validationMiddleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import { ZodError, AnyZodObject } from "zod";
+import type { ZodIssue } from "zod";
+
+import { StatusCodes } from "http-status-codes";
+
+export function validateData(schema: AnyZodObject) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const errorMessages = error.errors.map((issue: ZodIssue) => ({
+          message: `${issue.path.join(".")} is ${issue.message}`,
+        }));
+        res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ error: "Invalid data", details: errorMessages });
+      } else {
+        res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json({ error: "Internal Server Error" });
+      }
+    }
+  };
+}
