d0f96efc5f0b384b98e82c94dd5bb7055521c1fb
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-03-16 18:07:04 -0300

    add bson and make a prototype for users
    should work rn

--- mongo_test.ts
+++ mongo_test.ts
@@ -1,12 +1,24 @@
 import { PrismaClient } from '@prisma/client'
+import { ObjectId } from 'bson';
 
 const prisma = new PrismaClient()
 
 async function main() {
   // ... you will write your Prisma Client queries here
-  const allUsers = await prisma.users.findMany();
+  // GPT like yes.
+  const createUser = await prisma.user.create({
+    data: {
+      username: 'user100', email: 'user4@example.com', password: 'password1',
+    },
+  });
+
+  let id : string = createUser.id;
+
+  const friendlist = await prisma.friendlist.create({data: {userId : id}})
+  /*
+  const allUsers = await prisma.user.findMany();
   console.log(allUsers);
-  console.log("whats this?");
+  console.log("whats this?");*/
 }
 
 main()
