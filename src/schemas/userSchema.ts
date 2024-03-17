8eb904aef7c2b08e5a9f14a3a662b4d22e8de046
José <josesand99v@gmail.com>
2024-03-16 21:18:52 -0300

    Add user login functionality

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -5,3 +5,12 @@ export const userRegistrationSchema = z.object({
   email: z.string().email(),
   password: z.string().min(8),
 });
+
+
+export const userLoginSchema = z.object({
+  username: z.string().optional(),
+  email: z.string().email().optional(),
+  password: z.string().min(8),
+}).refine(data => data.email || data.username, {
+  message: "Either username or email is required",
+});
--- src/userRouter.ts
+++ src/userRouter.ts
@@ -1,8 +1,9 @@
 import { Router, Request, Response } from "express";
 import bcrypt from "bcrypt";
 import prisma from "./db/prisma";
+import jwt from "jsonwebtoken";
 import { validateData } from "./middleware/validationMiddleware";
-import { userRegistrationSchema } from "./schemas/userSchema";
+import { userRegistrationSchema, userLoginSchema } from "./schemas/userSchema";
 
 const userRouter = Router();
 
@@ -22,6 +23,42 @@ userRouter.post(
       })
       .then((user) => {
         res.json(user);
+      })
+      .catch((error) => {
+        res.json({ error: error.message });
+      });
+  },
+);
+
+userRouter.post(
+  "/login",
+  validateData(userLoginSchema),
+  (req: Request, res: Response) => {
+    const { username, email, password } = req.body;
+    prisma.user
+      .findFirst({
+        where: {
+          OR: [{ username }, { email }],
+        },
+      })
+      .then((user) => {
+        if (!user) {
+          res.status(404).json({ error: "User not found" });
+          return;
+        }
+        const isPasswordValid = bcrypt.compareSync(password, user.password);
+        if (!isPasswordValid) {
+          res.status(401).json({ error: "Invalid password" });
+          return;
+        }
+        const token = jwt.sign(
+          { id: user.id },
+          process.env.JWT_SECRET as string,
+          {
+            expiresIn: "1h",
+          },
+        );
+        res.json({ token });
       })
       .catch((error) => {
         res.json({ error: error.message });
