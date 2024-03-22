b799da970fd477181af75d1edfdddbf62ca74c03
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-03-21 22:50:35 -0300

    remove unused function (auth token)

--- src/middleware/jwtMiddleware.ts
+++ src/middleware/jwtMiddleware.ts
@@ -11,19 +11,6 @@ function generateAccessToken(username: string): string {
   return jwt.sign({ user: username }, secret, { expiresIn: 60 });
 }
 
-function authenticateToken(req: Request, res: Response, next: NextFunction) {
-  const token = req.headers.authorization;
-
-  if (token == null) return res.sendStatus(StatusCodes.UNAUTHORIZED);
-
-  const verify = jwt.verify(token, process.env.TOKEN_SECRET as string);
-  if (verify) {
-    next();
-  } else {
-    res.statusCode = StatusCodes.UNAUTHORIZED;
-  }
-}
-
 function authenticateJWTCookie(req: Request, res: Response, next: NextFunction) {
   const token = req.cookies.token;
 
@@ -39,4 +26,4 @@ function authenticateJWTCookie(req: Request, res: Response, next: NextFunction) 
 }
 
 
-export { generateAccessToken, authenticateToken, authenticateJWTCookie };
+export { generateAccessToken, authenticateJWTCookie };
