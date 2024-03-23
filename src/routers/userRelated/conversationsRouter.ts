19cd5e393f58e7fbf53b89ed39d3dc8a2121bb45
José <josesand99v@gmail.com>
2024-03-23 10:38:59 -0300

    Add authentication and validation to conversationsRouter plus send message because feels proper in this route.

--- src/routers/userRelated/conversationsRouter.ts
+++ src/routers/userRelated/conversationsRouter.ts
@@ -1,67 +1,76 @@
 import { Router, Request, Response } from "express";
 import prisma from "../../db/prisma";
 import { StatusCodes } from "http-status-codes";
+import { validateRequest } from "zod-express-middleware";
+import { sendMessageSchema } from "../../schemas/messagesSchema";
+
+import { io } from "../..";
+import { authenticateJWTCookie } from "../../middleware/jwtMiddleware";
 
 export const convRouter = Router({ mergeParams: true });
 
-convRouter.get("/", async (req: Request, res: Response) => {
-  if (!req.params.username) {
-    const conversations = await prisma.conversation.findMany({
-      select: {
-        id: true,
-        participants: {
-          //we only care abt the username not the other weird stuff.
-          select: {
-            username: true,
+convRouter.get(
+  "/",
+  authenticateJWTCookie,
+  async (req: Request, res: Response) => {
+    if (!req.params.username) {
+      const conversations = await prisma.conversation.findMany({
+        select: {
+          id: true,
+          participants: {
+            //we only care abt the username not the other weird stuff.
+            select: {
+              username: true,
+            },
+          },
+          messages: {
+            select: {
+              content: true,
+              //this may look better one-lined.
+              sender: { select: { username: true } },
+              createdAt: true,
+            },
+            orderBy: {
+              createdAt: "asc",
+            },
           },
         },
-        messages: {
-          select: {
-            content: true,
-            //this may look better one-lined.
-            sender: { select: { username: true } },
-            createdAt: true,
+      });
+      res.json(conversations);
+    } else {
+      const conversations = await prisma.conversation.findMany({
+        select: {
+          id: true,
+          participants: {
+            //we only care abt the username not the other weird stuff.
+            select: {
+              username: true,
+            },
           },
-          orderBy: {
-            createdAt: "asc",
-          },
-        },
-      },
-    });
-    res.json(conversations);
-  } else {
-    const conversations = await prisma.conversation.findMany({
-      select: {
-        id: true,
-        participants: {
-          //we only care abt the username not the other weird stuff.
-          select: {
-            username: true,
-          },
-        },
-        messages: {
-          select: {
-            content: true,
-            //this may look better one-lined.
-            sender: { select: { username: true } },
-            createdAt: true,
-          },
-          orderBy: {
-            createdAt: "asc",
+          messages: {
+            select: {
+              content: true,
+              //this may look better one-lined.
+              sender: { select: { username: true } },
+              createdAt: true,
+            },
+            orderBy: {
+              createdAt: "asc",
+            },
           },
         },
-      },
-      where: {
-        participants: {
-          some: {
-            username: req.params.username,
+        where: {
+          participants: {
+            some: {
+              username: req.params.username,
+            },
           },
         },
-      },
-    });
-    res.json(conversations);
-  }
-});
+      });
+      res.json(conversations);
+    }
+  },
+);
 
 convRouter.get("/:convId", async (req: Request, res: Response) => {
   const convId = req.params.convId;
@@ -96,3 +105,51 @@ convRouter.get("/:convId", async (req: Request, res: Response) => {
 
   res.json(conversation);
 });
+
+convRouter.post(
+  "/:convId/send",
+  validateRequest(sendMessageSchema),
+  authenticateJWTCookie,
+  async (req, res) => {
+    //retrieve the sender from the url
+    const sender = req.params.username;
+    const convId = req.params.convId;
+    //retrieve the message from the body
+    const { message } = req.body;
+    const conversation = await prisma.conversation.findFirst({
+      where: {
+        id: convId,
+        participants: {
+          some: {
+            username: sender,
+          },
+        },
+      },
+    });
+    if (!conversation) {
+      //conversation must exist before sending a message
+      res.json({ error: "Conversation does not exist" });
+      return;
+    }
+    //create the message
+    await prisma.message.create({
+      data: {
+        content: message,
+        sender: {
+          connect: {
+            username: sender,
+          },
+        },
+        conversation: {
+          connect: {
+            id: convId,
+          },
+        },
+      },
+    });
+    //emit the message
+    io.to(convId).emit("message", { sender, message });
+
+    res.json({ message: "Message sent" });
+  },
+);
