f54e4ddfc9bc731c0cd2a1038c03d318f2174076
José <josesand99v@gmail.com>
2024-03-23 14:05:59 -0300

    Update app entrypoints using react router

--- wasap2frontend/src/main.tsx
+++ wasap2frontend/src/main.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { NextUIProvider } from "@nextui-org/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root.tsx";
+import ErrorPage from "./routes/errorPage.tsx";
+import Contact from "./routes/contact.tsx";
+import LoginPage from "./routes/auth/login.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/contacts/:contactid",
+        element: <Contact />,
+        errorElement: <ErrorPage />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <RouterProvider router={router} />
+    </NextUIProvider>
   </React.StrictMode>,
-)
+);
