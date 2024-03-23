9aaabc856d58a4b9ca2cc9f6bdc6aeb6a128163e
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-03-22 21:47:46 -0300

    select the most important stuff on conversation

--- src/routers/userRelated/conversationsRouter.ts
+++ src/routers/userRelated/conversationsRouter.ts
@@ -6,6 +6,24 @@ export const convRoute = Router({mergeParams : true})
 
 convRoute.get("/", async (req : Request, res : Response) => {
     const conversations = await prisma.conversation.findMany({
+        select : {
+            id : true,
+            participants : {
+                //we only care abt the username not the other weird stuff.
+                select: {
+                    username : true,
+                }
+            },
+            messages: {
+                select: {
+                    content: true,
+                    //this may look better one-lined.
+                    sender: {select: {username: true}},
+                    createdAt : true,
+                }
+            },
+        },
     });
     res.json(conversations);
 })
+
