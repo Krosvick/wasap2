2612d27710bd202dd4a6cfe73cecd3ebd86b3cef
José <josesand99v@gmail.com>
2024-03-24 12:39:19 -0300

    Refactor routing logic and introduce Routes component

--- wasap2frontend/src/main.tsx
+++ wasap2frontend/src/main.tsx
@@ -4,38 +4,15 @@ import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./providers/reactQueryProvider.ts";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Routes from "./providers/routesProvider.tsx";
 
 //routes
-import Root from "./routes/root.tsx";
-import ErrorPage from "./routes/errorPage.tsx";
-import Contact from "./routes/contact.tsx";
-import LoginPage from "./routes/auth/login.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/contacts/:contactid",
-        element: <Contact />,
-        errorElement: <ErrorPage />,
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <NextUIProvider>
-        <RouterProvider router={router} />
+        <Routes />
       </NextUIProvider>
     </QueryClientProvider>
   </React.StrictMode>,
---
+++ wasap2frontend/src/providers/routesProvider.tsx
@@ -0,0 +1,52 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { useAuth } from "./authProvider";
+import { ProtectedRoute } from "../routes/protectedRoute";
+
+import Root from "../routes/root";
+import LoginPage from "../routes/auth/login";
+
+const Routes = () => {
+  const { token } = useAuth();
+
+  // Define public routes accessible to all users
+  const routesForPublic = [
+    {
+      path: "/about-us",
+      element: <div>About Us</div>,
+    },
+  ];
+
+  // Define routes accessible only to authenticated users
+  const routesForAuthenticatedOnly = [
+    {
+      path: "/",
+      element: <ProtectedRoute />, // Wrap the component in ProtectedRoute
+      children: [
+        {
+          path: "/",
+          element: <Root />,
+        },
+      ],
+    },
+  ];
+
+  // Define routes accessible only to non-authenticated users
+  const routesForNotAuthenticatedOnly = [
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+  ];
+
+  // Combine and conditionally include routes based on authentication status
+  const router = createBrowserRouter([
+    ...routesForPublic,
+    ...(!token ? routesForNotAuthenticatedOnly : []),
+    ...routesForAuthenticatedOnly,
+  ]);
+
+  // Provide the router configuration using RouterProvider
+  return <RouterProvider router={router} />;
+};
+
+export default Routes;
