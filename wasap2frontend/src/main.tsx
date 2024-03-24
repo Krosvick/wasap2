a8c308d8b9b4c7ee544849669d461e83da7aa943
José <josesand99v@gmail.com>
2024-03-24 14:41:17 -0300

    Add AuthProvider to main.tsx

--- wasap2frontend/src/main.tsx
+++ wasap2frontend/src/main.tsx
@@ -5,15 +5,18 @@ import { NextUIProvider } from "@nextui-org/react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./providers/reactQueryProvider.ts";
 import Routes from "./providers/routesProvider.tsx";
+import AuthProvider from "./providers/authProvider.tsx";
 
 //routes
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <NextUIProvider>
-        <Routes />
-      </NextUIProvider>
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <NextUIProvider>
+            <Routes />
+        </NextUIProvider>
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>,
 );
