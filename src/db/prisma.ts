f7f30498bd655e50e7ffc92088087eb0a57ed504
Franco <115720207+Francoo86@users.noreply.github.com>
2024-03-26 13:36:40 +0000

    singletonified prisma client

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,5 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+//const prisma = new PrismaClient();
 
+const SinglePrisma = () => {
+    return new PrismaClient();
+}
+
+declare global {
+    var prismaGlobal: undefined | ReturnType<typeof SinglePrisma> //eslint-disable-line no-var
+};
+
+const prisma = global.prismaGlobal ?? SinglePrisma();
 export default prisma;
