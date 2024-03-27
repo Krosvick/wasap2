31d34a075d9f4917733d9961da1035cf95719467
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-03-27 19:33:02 -0300

    fix the socket.join (async moment)
    also give more detailed info about whats happening.

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,10 @@ import { createServer } from "node:http";
 import { join } from "node:path";
 import { convRouter } from "./routers/userRelated/conversationsRouter";
 import cookieParser from "cookie-parser";
-import { LOG_TYPES, debugLogs, getCookieFromSocket, ISocketInfo} from "./helpers";
+import { LOG_TYPES, debugLogs, getCookieFromSocket} from "./helpers";
 import prisma from "./db/prisma";
 import { authenticateJWTCookie } from "./middleware/jwtMiddleware";
+import { leaveRoom, ISocketInfo } from "./chat_helpers";
 
 const VIEWS_DIR = join(__dirname, "..", "pseudoviews");
 
@@ -66,6 +67,9 @@ io.on('connection', (socket) => {
   //This should be the socket where the user enters conversation.
   //Should receive the conversation id.
   socket.on('enter-conversation', async (data) => {
+    //delete previous data if any.
+    storedUsers = leaveRoom(socket.id, storedUsers);
+  
     const convId = data.conversation;
     const userId = getCookieFromSocket(cookief);
     
@@ -74,10 +78,10 @@ io.on('connection', (socket) => {
     }
 
     //Make sure we joined the conversation.
-    socket.join(convId);
+    await socket.join(convId);
 
     //Send the status to clients in the 'Whatsapp-like' thing.
-    socket.to(convId).emit("user-status", {
+    socket.emit("user-status", {
       status : "connected",
     })
 
@@ -120,10 +124,12 @@ io.on('connection', (socket) => {
     }).then((user) => {
       let convTarget = storedUsers.find(sender => sender.userId === user?.id)?.convId;
 
-      if (convTarget)
-        io.to(convTarget).emit('chat message', {user : user?.username, message : data.message});
+      debugLogs(LOG_TYPES.INFO, `Trying to send a message to the room: ${convTarget} from ${user?.username}`);
 
-      debugLogs(LOG_TYPES.NONE, `User connected:  ${user?.username}`);
+      if (convTarget){
+        io.to(convTarget).emit('chat message', {user : user?.username, message : data.message});
+        debugLogs(LOG_TYPES.INFO, `Message Info: Room ${convTarget}, User: ${user?.username} Message: ${data.message}`);
+      }
     }).catch((err) => {
       console.log("error feo");
     });
@@ -134,6 +140,7 @@ io.on('connection', (socket) => {
   // Listen for user disconnection
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
+    storedUsers = leaveRoom(socket.id, storedUsers);
   });
 });
 
