7d7ec4e1202bf31ed571eefa202fdf81c8683570
José Sandoval <josesand99v@gmail.com>
2024-03-27 14:43:00 +0000

    add api url to vite config

--- wasap2frontend/vite.config.ts
+++ wasap2frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env": {
+      DEVELOPMENT_API_URL:
+        "https://crispy-spork-p5w99wrxp5w2rp4q-3000.app.github.dev/api",
+    },
+  },
+});
