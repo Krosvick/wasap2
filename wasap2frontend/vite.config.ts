e5b6e419beceaa82f18e00223eb18155197d2ace
José <josesand99v@gmail.com>
2024-03-28 12:19:21 -0300

    Update title in index.html and remove unused API URL

--- wasap2frontend/vite.config.ts
+++ wasap2frontend/vite.config.ts
@@ -10,8 +10,6 @@ export default defineConfig({
   define: {
     "process.env": {
       ...process.env,
-      DEVELOPMENT_API_URL:
-        "https://crispy-spork-p5w99wrxp5w2rp4q-3000.app.github.dev/api",
     },
   },
 });
