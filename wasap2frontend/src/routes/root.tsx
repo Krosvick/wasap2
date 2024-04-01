e0dca9126d20a55acf6b702d6c7efe421066b378
José <josesand99v@gmail.com>
2024-03-31 21:24:06 -0300

    add styling here and there lol

--- wasap2frontend/src/routes/root.tsx
+++ wasap2frontend/src/routes/root.tsx
@@ -27,7 +27,7 @@ export default function Root() {
     <main className="flex w-screen h-screen">
       <ToastContainer />
       <div className="flex flex-col justify-between w-1/4 h-full bg-emerald-600 p-5 text-zinc-100">
-        <div className="flex flex-col max-h-full">
+        <div className="flex flex-col justify-between h-full max-h-full">
           <AddContactForm userData={data} />
           {isLoading && <p>Loading...</p>}
           {isSuccess && <Contacts userId={userId!} />}
