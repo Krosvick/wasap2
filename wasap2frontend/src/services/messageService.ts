2d9f608a67401c272b31facc11d5af376c802243
José <josesand99v@gmail.com>
2024-03-31 21:24:13 -0300

    encryption demo?

--- wasap2frontend/src/services/messageService.ts
+++ wasap2frontend/src/services/messageService.ts
@@ -1,7 +1,20 @@
-import { sendMessage } from "../components/sendMessage";
 import socket from "../providers/socketioProvider";
+import E2EE from "@chatereum/react-e2ee";
 
-export const emitMessage = async (data: sendMessage) => {
+export const emitMessage = async (data: {
+  message: string;
+  receiverId: string;
+  conversationId: string;
+}) => {
+  const encryptedMessage = await E2EE.encryptPlaintext({
+    public_key: import.meta.env.VITE_PUBLIC_KEY,
+    plain_text: data.message,
+  });
+  console.log("Encrypted message: ", encryptedMessage);
+  const decryptedMessage = await E2EE.decryptForPlaintext({
+    private_key: import.meta.env.VITE_PRIVATE_KEY,
+    encrypted_text: encryptedMessage,
+  });
+  console.log("Decrypted message: ", decryptedMessage);
   socket.emit("send-message", data);
 };
-
